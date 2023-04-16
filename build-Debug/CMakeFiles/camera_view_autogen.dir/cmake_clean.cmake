f8d0f02a7c7380972745bb082309c999c4cefb38
Juhyung-L <102873080+Juhyung-L@users.noreply.github.com>
2023-04-16 16:45:03 -0400

    Delete build-Debug directory

--- build-Debug/CMakeFiles/camera_view_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/camera_view_autogen"
-  "camera_view_autogen/mocs_compilation.cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/camera_view_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
